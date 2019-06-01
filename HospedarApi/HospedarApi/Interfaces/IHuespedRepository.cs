65142e81713a019d59eb714e5ee7b848805f45b9
Jyc Yam <abc5pem@gmail.com>
2019-06-01 12:32:55 -0500

    Pruebas con Base de datos

---
+++ HospedarApi/HospedarApi/Interfaces/IHuespedRepository.cs
@@ -0,0 +1,13 @@
+﻿using HospedarApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospedarApi.Interfaces
+{
+    interface IHuespedRepository
+    {
+        Task<List<Huesped>> Listar();
+    }
+}
---
+++ HospedarApi/HospedarApi/Repository/HuespedRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using HospedarApi.Interfaces;
+using HospedarApi.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospedarApi.Repository
+{
+    public class HuespedRepository : IHuespedRepository
+    {
+        private readonly IConfiguration _config;
+
+        public HuespedRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("cn"));
+            }
+        }
+
+        public async Task<List<Huesped>> Listar()
+        {
+            List<Huesped> lis = new List<Huesped>();
+            IDbConnection conn = Connection;
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                lis = (await conn.QueryAsync<Huesped>("LISTAR_PERSONAS", parameters, commandType: CommandType.StoredProcedure)).ToList();
+            }
+            catch (Exception e)
+            {
+                var rpta = "Excepción encontrada: " + e.Message;
+                lis = null;
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return lis;
+        }
+    }
+}
