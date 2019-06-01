b1e25a2ef9b961f215f106095403f587261a637c
davidsmz <davidsmz693@gmail.com>
2019-06-01 14:38:19 -0500

    register host html

--- HospedarWeb/src/app/app-routing.module.ts
+++ HospedarWeb/src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Components
 import { HomeComponent } from './components/home/home.component';
+import { RegisterhostComponent } from './components/registerhost/registerhost.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'registerhost',
+    component: RegisterhostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
