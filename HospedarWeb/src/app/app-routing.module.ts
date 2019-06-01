118f3a3c90cca787f9e9875a9b23289a584b3fb9
davidsmz <davidsmz693@gmail.com>
2019-06-01 14:59:56 -0500

    navbar routing

--- HospedarWeb/src/app/app-routing.module.ts
+++ HospedarWeb/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 // Components
 import { HomeComponent } from './components/home/home.component';
 import { RegisterhostComponent } from './components/registerhost/registerhost.component';
+import { GuideComponent } from './components/guide/guide.component';
+import { AngelhostComponent } from './components/angelhost/angelhost.component';
+import { HostComponent } from './components/host/host.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'guide', component: GuideComponent },
+  { path: 'angelhost', component: AngelhostComponent },
+  { path: 'host', component: HostComponent },
   {
     path: 'registerhost',
     component: RegisterhostComponent
-  }
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
